55ea78014f0a6611449d587bb9dc80456bb6d5aa
Andrew Jeffery <dev@jeffas.io>
2023-01-24 22:21:38 +0000

    Add edit test

---
+++ crates/papers-cli/tests/edit.rs
@@ -0,0 +1,25 @@
+mod common;
+use common::Fixture;
+use expect_test::expect;
+
+#[test]
+fn test_help() {
+    let mut f = Fixture::new();
+    f.check_ok(
+        "edit --help",
+        expect![[r#"
+            Edit a paper's metadata in an editor
+
+            Usage: papers edit [OPTIONS] <ID>
+
+            Arguments:
+              <ID>  Id of the paper to edit
+
+            Options:
+              -c, --config-file <CONFIG_FILE>    Config file path to load
+                  --default-repo <DEFAULT_REPO>  Default repo to use if not found in parents of current directory
+                  --db-filename <DB_FILENAME>    Filename for the database
+              -h, --help                         Print help information"#]],
+        expect![""],
+    );
+}
